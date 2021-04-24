a8ea79a066a8ac00278e19a86625f93d1e0515dc
AngelaSophia <79142282+AngelaSophia@users.noreply.github.com>
2021-04-24 12:51:07 +0300

    Add files via upload

---
+++ GREEN.ino
@@ -0,0 +1,5 @@
+void  GREEN(){
+digitalWrite (Green,HIGH);
+  delay    (4000);
+  digitalWrite(Green,LOW);
+  }
---
+++ main.ino
@@ -0,0 +1,26 @@
+int Red=12;
+int Green=13;
+int Amber=14;
+
+
+void setup(){
+  SETUP();
+//  pinMode   (RED,OUTPUT);
+//  pinMode   (GREEN,OUTPUT);
+//  pinMode   (AMBER,OUTPUT);
+}
+
+void loop(){
+RED ();
+GREEN();
+AMBER();  
+//  digitalWrite(RED,HIGH);
+//  delay   (5000);
+//  digitalWrite(RED,LOW);
+//  digitalWrite (GREEN,HIGH);
+//  delay    (4000);
+//  digitalWrite(GREEN,LOW);
+//  digitalWrite(AMBER,HIGH);
+//  delay    (5000);
+//  digitalWrite(AMBER,LOW);
+}
